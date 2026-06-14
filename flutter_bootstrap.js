39f94e92e0b5993ca12d5a5c68b7663d4dc66ff1
sagarmathabytes <sagarmathabytes@gmail.com>
2026-06-14 12:05:54 +0545

    updating the call function

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "593652753" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "374792130" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
