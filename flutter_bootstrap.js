d22fcf3aa8c4d87a2a367c1b17ebf87b17e7a21a
sagarmathabytes <sagarmathabytes@gmail.com>
2026-06-30 11:25:50 +0545

    some minor updates to looks and usability for web and also mobile web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1469516685" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "601568533" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
